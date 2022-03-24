2a315cebfd114faf433cec2a63a3bcc831c128ac
MelvinLopez00089 <98369987+MelvinLopez00089@users.noreply.github.com>
2022-03-23 21:11:52 -0600

    Tarea#8
    main.RaizCuadrada

---
+++ RaizCuadrada.main.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+#include <cmath> 
+  
+  
+  //Melvin yovani Lopez
+using namespace std; 
+  
+int main() { 
+  float dato,raiz; 
+  
+  cout << "Ingrese un numero para obtener la raiz cuadrada:"; 
+  cin >> dato; 
+  
+  raiz = sqrt(dato); 
+  
+  cout << "La raiz cuadrada de " << dato << " es " << raiz << endl; 
+  
+  return 0; 
+} 
